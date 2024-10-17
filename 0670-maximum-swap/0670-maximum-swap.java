5614f0e4ab04a2158e2275df6665b72ea6f65e60
Rishu-Chauhan <rishuchauhan798@gmail.com>
2024-10-17 23:39:33 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (53.17%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumSwap(int num) {
+       char[] digits = Integer.toString(num).toCharArray();
+        int[] lastIndex = new int[10];
+
+        for (int i = 0; i < digits.length; i++) {
+            lastIndex[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int d = 9; d > digits[i] - '0'; d--) {
+                if (lastIndex[d] > i) {
+                    char temp = digits[i];
+                    digits[i] = digits[lastIndex[d]];
+                    digits[lastIndex[d]] = temp;
+                    return Integer.parseInt(new String(digits));
+                }
+            }
+        }
+        
+        return num;  
+    }
+}
